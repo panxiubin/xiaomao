22870d2a6594a34928becde5806233de83190834
panxiubin <xiubinpan@gmail.com>
2016-10-31 03:11:15 +0800

    add api controller

---
+++ app/controllers/api/v1/teachers_controller.rb
@@ -0,0 +1,28 @@
+module Api
+  module V1
+    class TeachersController < ApplicationController
+      def index
+        infors = []
+        @teachers = Teacher.all
+        @teachers.each do |teacher|
+          teacher_name = teacher.name
+          @courses = teacher.courses
+          courses = []
+          @courses.each do |course|
+            course_name = course.name
+            courses << course_name
+          end
+          info = {"teacher" => teacher_name, "courses" => courses}
+          infors << info
+        end
+        render json: infors
+      end
+
+      def destroy
+        @teacher = Teacher.find(params[:id])
+        @teacher.destroy
+      end
+
+    end
+  end
+end
