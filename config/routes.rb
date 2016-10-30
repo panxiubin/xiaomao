9939ea1ecfa2f9e151fe030e8cfdb2be61863e14
panxiubin <xiubinpan@gmail.com>
2016-10-31 02:55:01 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   resources :students, only: :show
   resources :teachers, only: [:index, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, path: '/' do
+  	namespace :v1 do
+      resources :students, only: :show
+      resources :teachers
+    end
+  end
+
+  resources :welcome, only: :index
+
 end
